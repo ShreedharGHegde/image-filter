13d50ab24d30e6c334e545af3035c1e617261cc2
Shreedhar Hegde <shreedharhegade96@gmail.com>
2019-06-18 10:58:25 +0530

    first-commit

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,20 +1,31 @@
-import React, { Component, Image } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
 export default class Gallery extends Component {
   state = {
-    file: null,
+    floorUrl: null,
     image: null,
     gallery: [],
-    colorize: false,
     floortest: false,
+    wholeRoom: false,
     imgUploaded: false,
-    resultUrl: false
+    resultUrl: false,
+    urls: false
   };
 
   onChange = e => {
     this.setState({
-      [e.target.name]: e.target.checked
+      [e.target.name]: e.target.value
+    });
+  };
+
+  floorOnlyHandle = e => {
+    this.setState({});
+  };
+
+  urlHandle = e => {
+    this.setState({
+      [e.target.name]: e.target.value
     });
   };
 
@@ -24,25 +35,32 @@ export default class Gallery extends Component {
     });
   };
   onSubmit = e => {
+    console.log(this.state);
+    this.setState({
+      loading: true
+    });
     e.preventDefault();
     const formData = new FormData();
-    formData.append("file", this.state.file);
-    formData.append("colorize", this.state.colorize);
+    formData.append("floortestUrl", this.state.floorUrl);
     formData.append("floortest", this.state.floortest);
     // formData.append("colorize", this.state.)
     axios
       .post("http://localhost:5000/upload", formData)
       .then(res => {
-        console.log(res.data);
-        this.setState(
-          {
-            resultUrl: `https://algorithmia.com/v1/data/${res.data.resultUrl}`,
-            imgUploaded: res.data.imgUploaded
-          },
-          () => {
-            console.log(this.state.resultUrl);
-          }
-        );
+        console.log("res", res.data);
+        if (res.data.result) {
+          this.setState(
+            {
+              urls: res.data.result
+            },
+            () => {}
+          );
+          // } else if (this.state.resultUrl) {
+          //   this.setState({
+          //     resultUrl: `https://algorithmia.com/v1/data/${res.data.resultUrl}`,
+          //     imgUploaded: res.data.imgUploaded
+          //   });
+        }
       })
       .catch(err => {
         console.log(err);
@@ -50,22 +68,35 @@ export default class Gallery extends Component {
   };
 
   render() {
-    // let photos = this.state.gallery.map(imgName => {
-    //   return <img src={require(`../uploads/${imgName}`)} alt="" />;
-    // });
+    console.log("urls", this.state.urls);
+    let floorImages = this.state.urls
+      ? this.state.urls.map(url => {
+          return (
+            <img
+              src={url}
+              alt=""
+              key={url}
+              style={{
+                width: 200,
+                height: 200,
+                paddingLeft: 20,
+                paddingTop: 20
+              }}
+            />
+          );
+        })
+      : null;
 
-    console.log(this.state.gallery);
     return (
       <div>
-        <div>Upload Image</div>
+        <div>
+          <h3>Algorithmia</h3>
+        </div>
 
         <form onSubmit={this.onSubmit}>
-          <input type="file" onChange={this.imageHandler} />{" "}
-          <button type="submit" value="submit">
-            Upload
-          </button>
+          {/* <input type="file" onChange={this.imageHandler} />{" "} */}
           <br />{" "}
-          <input
+          {/* <input
             type="checkbox"
             name="colorize"
             checked={this.state.checked}
@@ -73,25 +104,48 @@ export default class Gallery extends Component {
             id="colorize"
           />
           colorize
-          <br />{" "}
-          <input
-            type="checkbox"
-            name="floortest"
-            onChange={this.onChange}
-            checked={this.state.checked}
-            id="foortest"
-          />
-          floor test
+          <br />{" "} */}
+          <div>
+            Insert url of an image for floor detection and select one of the
+            filters
+          </div>
+          <div>
+            <input type="text" name="floorUrl" onChange={this.urlHandle} />
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="floortest"
+              value="flooronly"
+              checked={this.state.checked}
+              onChange={this.onChange}
+              style={{ marginLeft: 30 }}
+            />{" "}
+            floor only
+            <input
+              type="radio"
+              name="floortest"
+              value="wholeroom"
+              checked={this.state.checked}
+              onChange={this.onChange}
+            />{" "}
+            whole room
+          </div>
+          <div style={{ paddingTop: 10 }}>
+            <button type="submit" value="submit">
+              Detect floors
+            </button>
+          </div>
         </form>
 
         <div className="container">
           <div className="row">
             <div className="col-lg-3">
-              {this.state.imgUploaded ? (
+              {this.state.floorUrl ? (
                 <div>
-                  <p>Image you uploaded</p>
+                  <h4>Image from URL</h4>
                   <img
-                    src={require(`../uploads/${this.state.imgUploaded}`)}
+                    src={this.state.floorUrl}
                     alt="new"
                     height="200px"
                     width="300px"
@@ -112,6 +166,14 @@ export default class Gallery extends Component {
                 </div>
               ) : null}
             </div>
+            {floorImages ? (
+              <div style={{ paddingTop: 30 }}>
+                <div>
+                  <h4>Detected floors </h4>
+                </div>{" "}
+                {floorImages}{" "}
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
--- server.js
+++ server.js
@@ -13,103 +13,139 @@ app.use(cors());
 let imageNames = [];
 var images = client.dir("data://ShreedharGHegde/images");
 let img_file;
+let uploadedFile;
+// let crateDic = () => {
+//   return new Promise((resolve, reject) => {
+//     images.exists(function(exists) {
+//       if (exists == false) {
+//         images.create(function(response) {
+//           if (response.error) {
+//             return console.log(
+//               "Failed to create dir: " + response.error.message
+//             );
+//           } else {
+//             console.log("Created directory: " + images.data_path);
+//           }
+//         });
+//       } else {
+//         console.log("Your directory already exists.");
+//       }
+//       resolve(true);
+//     });
+//   });
+// };
 
-let crateDic = () => {
-  return new Promise((resolve, reject) => {
-    images.exists(function(exists) {
-      if (exists == false) {
-        nlp_directory.create(function(response) {
-          if (response.error) {
-            return console.log(
-              "Failed to create dir: " + response.error.message
-            );
-          } else {
-            console.log("Created directory: " + nlp_directory.data_path);
-          }
-        });
-      } else {
-        console.log("Your directory already exists.");
-      }
-      resolve(true);
-    });
-  });
-};
+// let uploadFile = fileName => {
+//   return new Promise((resolve, reject) => {
+//     var local_file = `/home/shreedhar/gallery/client/src/uploads/${fileName}`;
 
-let uploadFile = fileName => {
-  return new Promise((resolve, reject) => {
-    var local_file = `/home/shreedhar/gallery/client/src/uploads/${fileName}`;
+//     img_file = `data://ShreedharGHegde/images/${fileName}`;
+//     console.log("000", img_file);
 
-    img_file = `data://ShreedharGHegde/images/${fileName}`;
-    console.log("000", img_file);
+//     client.file(img_file).exists(function(exists) {
+//       // Check if file exists, if it doesn't create it
+//       if (exists == false) {
+//         images.putFile(local_file, function(response) {
+//           if (response.error) {
+//             return console.log(
+//               "Failed to upload file: " + response.error.message
+//             );
+//             reject(false);
+//           } else {
+//             console.log("File uploaded.", response);
 
-    client.file(img_file).exists(function(exists) {
-      // Check if file exists, if it doesn't create it
-      if (exists == false) {
-        images.putFile(local_file, function(response) {
-          if (response.error) {
-            return console.log(
-              "Failed to upload file: " + response.error.message
-            );
-          } else {
-            console.log("File uploaded.");
-          }
-        });
-      } else {
-        console.log("Your file already exists.");
-      }
-    });
-    resolve(true);
-  });
-};
+//             resolve(response);
+//           }
+//         });
+//       } else {
+//         console.log("Your file already exists.");
+//         resolve(true);
+//       }
+//     });
+//   });
+// };
 
 app.post("/upload", (req, url) => {
-  if (req.files === null) {
-    return url.json({ msg: "no file uploaded" });
-  }
+  console.log("upload");
+  // if (req.files === null) {
+  //   return url.json({ msg: "no file uploaded" });
+  // }
 
-  const file = req.files.file;
-  file.mv(`${__dirname}/client/src/uploads/${file.name}`, err => {
-    if (err) {
-      console.log(err);
-      return err;
-    }
-    imageNames.push(file.name);
-    console.log(imageNames);
-    if (req.body.colorize) {
-      crateDic()
-        .then(result => {
-          return uploadFile(file.name);
-        })
-        .then(res => {
-          client.file(img_file).exists(function(exists) {
-            // Download contents of file as a string if it exists
-            console.log("====>", img_file);
+  // let file = null;
+  // if (req.files) {
+  //   file = req.files.file;
+  //   file.mv(`${__dirname}/client/src/uploads/${file.name}`, err => {
+  //     if (err) {
+  //       console.log(err);
+  //       return err;
+  //     }
+  //   });
+  //   imageNames.push(file.name);
+  // }
 
-            client.file(img_file).get(function(err, data) {
-              if (err) {
-                console.log("Failed to download file.");
-                console.log(err);
-              } else {
-                console.log("Successfully downloaded data.");
-              }
-              var input = data;
-
-              // Call an algorithm with text input by passing a string into the pipe method. The returned promise will be called with the response with the Algorithm completes (or when an error occurs). If the algorithm output is text, then the get() method on the response will return a string.
-
-              client
-                .algo("deeplearning/ColorfulImageColorization/1.1.13")
-                .pipe(input)
-                .then(function(response) {
-                  url.send({
-                    imgUploaded: file.name,
-                    resultUrl: response.get().output
-                  });
-                });
-            });
-          });
+  // if (req.body.colorize == "true" || req.body.floortestUrl !== "false") {
+   console.log('body',req.body)
+    if (req.body.floortestUrl !== "false") {
+      console.log("floortest");
+      let value
+      if(req.body.floortest === 'flooronly'){
+        value = true
+      } else {
+        value = false
+      }
+      input = {
+        file: req.body.floortestUrl,
+        floor_only: value
+      };
+      console.log('input',input);
+      client
+        .algo("godmode/flooring_detection/0.1.2")
+        .pipe(input)
+        .then(function(response) {
+          console.log(response);
+          
+          url.send(response.get());
         });
     }
-  });
-});
+
+    // crateDic()
+    //   .then(result => {
+    //     return uploadFile(file.name);
+    //   })
+    //   .then(res => {
+    //     client.file(img_file).exists(function(exists) {
+    //       // Download contents of file as a string if it exists
+    //       console.log("====>", img_file);
+
+    //       client.file(img_file).get(function(err, data) {
+    //         if (err) {
+    //           console.log("Failed to download file.");
+    //           console.log(err);
+    //         } else {
+    //           console.log("Successfully downloaded data.");
+    //         }
+    //         var input = data;
+    //         // let result = {};
+    //         // Call an algorithm with text input by passing a string into the pipe method. The returned promise will be called with the response with the Algorithm completes (or when an error occurs). If the algorithm output is text, then the get() method on the response will return a string.
+    //         if (req.body.colorize == "true") {
+    //           console.log("colorize");
+    //           client
+    //             .algo("deeplearning/ColorfulImageColorization/1.1.13")
+    //             .pipe(input)
+    //             .then(function(response) {
+    //               // url.send({
+    //               //   imgUploaded: file.name,
+    //               //   resultUrl: response.get().output
+    //               // });
+    //               // result.imgUploaded = file.name;
+    //               // result.resultUrl = response.get().output
+    //               console.log("colorize response", response.get());
+    //             });
+    //         }
+    //       });
+    //     });
+    //   });
+  // }
+  })
 
 app.listen(5000, () => console.log("server started on port 5000"));
